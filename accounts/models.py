71e033a685d96c69b32e89be49d1ccb87635916a
rishabhgeete <rishabhgeete123@gmail.com>
2021-08-07 18:51:14 +0530

    phone login

--- accounts/models.py
+++ accounts/models.py
@@ -5,8 +5,8 @@ from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(_('email address'), unique=True)
+    phone = models.CharField(_('Phone number'), unique=True,max_length=10)
     
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'phone'
     REQUIRED_FIELDS = []
